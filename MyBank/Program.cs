f57a1346425b89045c674ab4245a5de625a7d32f
Manuele Sapia <manuele.sapia.comprend@outlook.com>
2021-02-01 08:53:53 +0000

    ....

--- MyBank/Program.cs
+++ MyBank/Program.cs
@@ -10,6 +10,20 @@ namespace MyBank
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}. ");
             account.MakeWithdrawal(120, DateTime.Now, "Hammock");
             Console.WriteLine(account.Balance);
+
+            try
+            {
+                var inalidAccount = new BankAccount("invalid", -55);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("excption with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            account.MakeDeposit(-300, DateTime.Now, "stealing");
+            Console.WriteLine(account.Balance);
+
         }
     }
 }
