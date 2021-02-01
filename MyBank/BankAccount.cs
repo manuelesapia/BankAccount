c4b6614becf39d269af88670deaf634e9071299a
manuele sapia <manulon12@outlook.com>
2021-02-01 07:36:52 +0000

    Add project files.

---
+++ MyBank/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBank
+{
+    class BankAccount
+    {
+
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+
+        public void MakeDeposit(decimal amount, DateTime date, string note){
+
+
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+
+        }
+
+    }
+}
